de686e41ddf94b796a48361a017bb2972c7f7df9
Code Sandbox <hello@codesandbox.io>
2022-10-11 13:45:36 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b7cbebfc0006901e2703a1e16560dcd4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8a844cf2c469563632baf2843cd5ff",
+    "url": "https://obiijeoma32.github.io/csb-dvt3z6/index.html"
+  },
+  {
+    "revision": "d6fc6a5adc5db742aca0",
+    "url": "https://obiijeoma32.github.io/csb-dvt3z6/static/js/2.25f74dec.chunk.js"
+  },
+  {
+    "revision": "ba8d0776adee49d8dbb3",
+    "url": "https://obiijeoma32.github.io/csb-dvt3z6/static/js/main.601a2828.chunk.js"
+  },
+  {
+    "revision": "967ca917c32477fc9bc4",
+    "url": "https://obiijeoma32.github.io/csb-dvt3z6/static/js/runtime-main.e0987335.js"
+  }
+]);
